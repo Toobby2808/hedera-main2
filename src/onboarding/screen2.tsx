e7da26ae41a7ee84548ebd8b39ea4b1d6f1dd852
Adewale Adesanya <adesanyaadewale89@gmail.com>
2025-10-31 15:52:15 -0700

    Added few features

--- src/onboarding/screen2.tsx
+++ src/onboarding/screen2.tsx
@@ -1,28 +1,23 @@
-
 import Onboarding from "./shared/hero";
-import { ellipse } from "../assets/images";
-import { useNavigate } from 'react-router-dom';
+
+import Image1 from "../assets/images-and-icons/screen2.svg";
+import { useNavigate } from "react-router-dom";
 
 export default function Screen2() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleGetStarted = () => {
-        console.log("Button clicked, attempting to navigate");
-        navigate('/signup');
-    };
+  const handleGetStarted = () => {
+    console.log("Button clicked, attempting to navigate");
+    navigate("/signup");
+  };
 
-
-    return (
-        <div className="h-screen bg-gradient-to-br from-emerald-50 to-teal-50 w-full flex flex-col justify-between">
-            <Onboarding
-                title="Discover New Moments Within Campus And Others"
-                image={ellipse}
-                onButtonClick={handleGetStarted}
-
-
-            />
-
-
-        </div>
-    );
+  return (
+    <div className="h-screen bg-linear-to-br from-emerald-50 to-teal-50 w-full flex flex-col justify-between">
+      <Onboarding
+        title="Discover New Moments Within Campus And Others"
+        image={Image1}
+        onButtonClick={handleGetStarted}
+      />
+    </div>
+  );
 }
