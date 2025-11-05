fa45eb9508dc5e808ae5074d649693ec33b908c1
Adewale Adesanya <adesanyaadewale89@gmail.com>
2025-11-05 01:29:06 -0800

    Debugged some issues

--- src/store/hashconnectSlice.ts
+++ src/store/hashconnectSlice.ts
@@ -1,4 +1,4 @@
-
+/* 
 import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 
 interface HashConnectState {
@@ -36,4 +36,40 @@ const hashconnectSlice = createSlice({
 
 
 export const { setLoading, setConnected, setDisconnected } = hashconnectSlice.actions;
+export default hashconnectSlice.reducer; */
+
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+interface HashconnectState {
+  isConnected: boolean;
+  accountId: string | null;
+  isLoading: boolean;
+}
+
+const initialState: HashconnectState = {
+  isConnected: false,
+  accountId: null,
+  isLoading: false,
+};
+
+const hashconnectSlice = createSlice({
+  name: "hashconnect",
+  initialState,
+  reducers: {
+    setConnected(state, action: PayloadAction<{ accountId: string }>) {
+      state.isConnected = true;
+      state.accountId = action.payload.accountId;
+    },
+    setDisconnected(state) {
+      state.isConnected = false;
+      state.accountId = null;
+    },
+    setLoading(state, action: PayloadAction<boolean>) {
+      state.isLoading = action.payload;
+    },
+  },
+});
+
+export const { setConnected, setDisconnected, setLoading } =
+  hashconnectSlice.actions;
 export default hashconnectSlice.reducer;
